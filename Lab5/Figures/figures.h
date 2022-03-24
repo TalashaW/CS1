0ade8aaef437a98679e9bb0a4f4cd8f298e42128
TalashaW <78000772+TalashaW@users.noreply.github.com>
2022-03-23 22:35:59 -0400

    Delete figures.h

--- Lab5/Figures/figures.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using std::cin; using std::cout; using std::endl;
-
-void square(int n);
-void slash(int n);
-void backslash(int n);
-void hollowsquare(int n);
-void slashedsquare(int n);
